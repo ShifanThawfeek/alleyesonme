86fe27b16f0c4ab468c9646a3c3426558c242ca6
ShifanThawfeek <mshifan76@gmail.com>
2021-08-18 19:29:17 +0530

    added Getting_started_with_MataOnMe page + LinkPage name is made visible by placing inside a white bg color button

---
+++ resources/views/public/getting_started.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.public')
+
+@section('content')
+
+<div class="container-fluid hero">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <p style="font-weight:bold; font-size: 35px;">Getting Started</p>
+        <ul>
+          <li>Verifying your account</li>
+          <li>How to create a MataOnMe account</li>
+          <li>How to create a link on your MataOnMe</li>
+          <li>Sharing your MataOnMe</li>
+          <li>Update your profile picture and background</li>
+          <li>Using your Analytics</li>
+          <li>MataOnMe Pro features overview</li>
+          <li>Be a part of the MataOnMe community</li>
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <p style="font-weight: bold; font-size: 35px">Account Basics</p>
+        <ul>
+          <li>Changing your email address</li>
+          <li>Where can I find my Linktree username</li>
+          <li>Moving your links</li>
+          <li>Archiving your links</li>
+          <li>Deleting your links </li>
+          <li>I have forgotton my password</li>
+         
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
